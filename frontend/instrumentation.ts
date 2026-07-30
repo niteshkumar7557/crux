03deef5f96fa7cbfdc38bb39e837de9f2bdf0bc1
niteshkumar7557 <devnitesh2008@gmail.com>
2026-07-30 19:17:05 +0530

    fix(sentry): sentry for edge runtime removed

--- frontend/instrumentation.ts
+++ frontend/instrumentation.ts
@@ -2,10 +2,13 @@ import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./sentry.server.config");
-  }
-  if (process.env.NEXT_RUNTIME === "edge") {
-    await import("./sentry.edge.config");
+    Sentry.init({
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+      enabled:
+        process.env.NODE_ENV === "production" &&
+        !!process.env.NEXT_PUBLIC_SENTRY_DSN,
+      tracesSampleRate: 0,
+    });
   }
 }
 
